4da01747902ce09ad73ee9399818fd3ec5c0e9c4
Oleksandr Lapin <oleksandrlapin7@gmail.com>
2025-01-20 20:55:36 +0100

    Initial commit without previous history

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
+    stripePublicKey: 'pk_test_51QiLIKA3uiTHG04P7Wb9ZTpelqDZsKmqWacnyesd0yFFzWWMfuTE6YnyQx9lrrxvrXipTjxgtb7o0DwIYx5TFGRW00PgEAHJAP'
+};
+
